a57adf96440d172179b3b5e0dd190bb9536ac4bd
Kathy <Kathy.wazowski@gmail.com>
2024-11-21 22:08:41 +0800

    parameterized queries

--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import usePosts from './hooks/usePosts';
 
 interface Post {
@@ -8,13 +9,27 @@ interface Post {
 }
 
 const PostList = () => {
-  const {data: posts, error, isLoading} = usePosts();
+  const [userId, setUserId] = useState<number>()
+  const {data: posts, error, isLoading} = usePosts(userId);
 
   if (isLoading) return <p>Loading...</p>
 
   if (error) return <p>{error.message}</p>;
 
   return (
+   <>
+    <select className="form-select mb-3" 
+    value={userId || ''}
+    onChange={event => {
+      const value = event.target.value;
+      setUserId(value ? parseInt(value) : undefined )
+    }}
+    >
+      <option value="">All</option>
+      <option value="1">User 1</option>
+      <option value="2">User 2</option>
+      <option value="3">User 3</option>
+    </select>
     <ul className="list-group">
       {posts?.map((post) => (
         <li key={post.id} className="list-group-item">
@@ -22,6 +37,7 @@ const PostList = () => {
         </li>
       ))}
     </ul>
+   </>
   );
 };
 
--- src/react-query/hooks/usePosts.ts
+++ src/react-query/hooks/usePosts.ts
@@ -8,14 +8,18 @@ interface Post {
   userId: number;
 }
 
-const usePosts = () => {
+const usePosts = (userId: number | undefined) => {
   const queryFn = () =>
     axios
-      .get<Post[]>("https://jsonplaceholder.typicode.com/posts")
+      .get<Post[]>("https://jsonplaceholder.typicode.com/posts", {
+        params: {
+          userId,
+        },
+      })
       .then((res) => res.data);
   // 在这里传入type 参数，打完<可以看到参数提醒
   return useQuery<Post[], Error>({
-    queryKey: ["posts"],
+    queryKey: userId ? ["users", userId, "posts"] : ["posts"],
     queryFn,
     staleTime: 10 * 1000, // 10s
   });
