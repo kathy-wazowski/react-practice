8aeadf195a440c6226df46a0ee8f81c16cccce67
Kathy <Kathy.wazowski@gmail.com>
2024-11-23 14:21:07 +0800

    addTodo.isLoading: add loading logic

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -55,7 +55,9 @@ const TodoForm = () => {
           <input ref={ref} type="text" className="form-control" />
         </div>
         <div className="col">
-          <button className="btn btn-primary">Add</button>
+          <button className="btn btn-primary" disabled={addTodo.isLoading}>
+            {addTodo.isLoading ? "Adding" : "Add"}
+          </button>
         </div>
       </form>
     </>
