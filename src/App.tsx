f4007be597b0e3a16edae84cc49fc1163b106879
Kathy <Kathy.wazowski@gmail.com>
2024-12-04 14:18:32 +0800

    Add: add counterStore using Zustand

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
         {/* <PostList /> */}
         {/* <TodoForm />
       <TodoList /> */}
-        {/* <Counter /> */}
+        <Counter />
         {/* <TaskList /> */}
         {/* <LoginStatus /> */}
         <NavBar />
