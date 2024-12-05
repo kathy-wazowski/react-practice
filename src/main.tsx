3e9f7f44b42d4feb0dfc905eaf82e0ca6feebebc
Kathy <Kathy.wazowski@gmail.com>
2024-12-05 19:39:24 +0800

    Add: set up routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import App from './App';
-import './index.css';
+import "bootstrap/dist/css/bootstrap.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import App from "./App";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./routing/routes";
 
 const globalQuerySetting = {
   defaultOptions: {
@@ -14,19 +16,17 @@ const globalQuerySetting = {
       staleTime: 10 * 1000, // 10s
       refetchOnWindowFocus: false,
       refetchOnReconnect: true,
-    }
-  }
-}
+    },
+  },
+};
 
 // you can pass in the globalQuerySetting
 const queryClient = new QueryClient();
 
-ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RouterProvider router={router} />
       <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>
